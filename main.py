b90f2c80147ad3bf4b930c5f98e4f6b3e9001331
Abu Ameen <abuameen.student@gmail.com>
2024-12-27 08:37:26 +0530

    Update withcount_word function

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     path_to_file = r"books/frankenstein.txt"
-    print(read_text(path_to_file))
+    text = read_text(path_to_file)
+    print(count_words(text))
+
+
+def count_words(sting):
+    list_of_words = sting.split()
+    return len(list_of_words)
 
 
 def read_text(book_path):
